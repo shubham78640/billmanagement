7af1f441060ec5fd104aec76111d25083a14dfdb
shubham78640 <shubham@thepinchlife.com>
2022-12-30 15:54:03 +0530

    updated

--- my-app/src/Pages/MainForm.js
+++ my-app/src/Pages/MainForm.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import ComponentAutoComplete from '../Component/ComponentAutoComplete'
 import Box from "@mui/material/Box";
-
+import TextField from '@mui/material/TextField';
 
 function MainForm() {
   return (
     <>
     <Box p={5} sx={{ display:"flex",gap:"20px" , flexWrap:'wrap', }}> 
-    
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+<TextField id="filled-basic" label="Filled" variant="filled" />
+<TextField id="standard-basic" label="Standard" variant="standard" />
+      <ComponentAutoComplete/>
+      <ComponentAutoComplete/>
+      <ComponentAutoComplete/>
+      <ComponentAutoComplete/>
+      <ComponentAutoComplete/>
+      <ComponentAutoComplete/>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+<TextField id="filled-basic" label="Filled" variant="filled" />
+<TextField id="standard-basic" label="Standard" variant="standard" />
       <ComponentAutoComplete/>
       <ComponentAutoComplete/>
       <ComponentAutoComplete/>
@@ -15,12 +26,9 @@ function MainForm() {
       <ComponentAutoComplete/>
       <ComponentAutoComplete/>
       <ComponentAutoComplete/>
-      <ComponentAutoComplete/>
-      <ComponentAutoComplete/>
-      <ComponentAutoComplete/>
-      <ComponentAutoComplete/>
-      <ComponentAutoComplete/>
-      <ComponentAutoComplete/>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+<TextField id="filled-basic" label="Filled" variant="filled" />
+<TextField id="standard-basic" label="Standard" variant="standard" />
     </Box>
     
     </>
@@ -28,3 +36,14 @@ function MainForm() {
 }
 
 export default MainForm ;
+
+
+const top100Films = [
+    { label: 'The Shawshank Redemption', year: 1994 },
+    { label: 'The Godfather', year: 1972 },
+    { label: 'The Godfather: Part II', year: 1974 },
+    { label: 'The Dark Knight', year: 2008 },
+    { label: '12 Angry Men', year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: 'Pulp Fiction', year: 1994 },
+]
